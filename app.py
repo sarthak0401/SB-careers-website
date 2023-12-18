f9651202d448d36516a33ce6c3e664c53b522bdc
sarthak0401 <120019344+sarthak0401@users.noreply.github.com>
2023-12-18 07:33:25 +0000

    added new job, render.com automatically starts new deployment based on the changes pushed to github.

--- app.py
+++ app.py
@@ -2,38 +2,36 @@ from flask import Flask, render_template, jsonify
 
 app = Flask(__name__)
 
-JOBS = [
-    {
-        'id': 1,
-        'title': 'Data Analyst',
-        'location': 'New York',
-        'salary': '$150,000'
-    },
-    {
-        'id': 2,
-        'title': 'Data scientist',
-        'location': 'New Delhi',
-        'salary': 'Rs. 15,00,000'
-    },
-    {
-        'id': 3,
-        'title': 'DevOps Engineer',
-        'location': 'Bengaluru',
-        # 'salary' : 'Rs. 20,00,000'
-    },
-    {
-        'id': 4,
-        'title': 'Cloud Engineer',
-        'location': 'Remote',
-        'salary': 'Rs. 21,00,0000'
-    },
-    {
-        'id': 5,
-        'title': 'Full-Stack Developer',
-        'location': 'San Francisco, USA',
-        'salary': '$120,000'
-    }
-]
+JOBS = [{
+    'id': 1,
+    'title': 'Data Analyst',
+    'location': 'New York',
+    'salary': '$150,000'
+}, {
+    'id': 2,
+    'title': 'Data scientist',
+    'location': 'New Delhi',
+    'salary': 'Rs. 15,00,000'
+}, {
+    'id': 3,
+    'title': 'DevOps Engineer',
+    'location': 'Bengaluru',
+}, {
+    'id': 4,
+    'title': 'Cloud Engineer',
+    'location': 'Remote',
+    'salary': 'Rs. 21,00,0000'
+}, {
+    'id': 5,
+    'title': 'Full-Stack Developer',
+    'location': 'San Francisco, USA',
+    'salary': '$120,000'
+}, {
+    'id': 6,
+    'title': 'Python Developer',
+    'location': 'Pune',
+    'salary': 'Rs. 8,00,000'
+}]
 
 
 @app.route("/")
